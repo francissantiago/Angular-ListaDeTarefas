3033b4c55b73ee13308a0f5c1476ba2cb57c9a64
Francis Santiago <francis1505@gmail.com>
2024-10-22 11:09:49 -0300

    :sparkles: Novo component - Input add item

---
+++ src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, inject, Output, ViewChild } from '@angular/core';
+
+// Interfaces
+import { IListItems } from '../../interface/IListItems.interface';
+
+@Component({
+  selector: 'app-input-add-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-add-item.component.html',
+  styleUrl: './input-add-item.component.scss'
+})
+export class InputAddItemComponent {
+  #cdr = inject(ChangeDetectorRef);
+
+  @ViewChild('inputText') public inputText!: ElementRef;
+
+  @Output() public outputListItems = new EventEmitter<IListItems>();
+  public focusAndAddItem(value: string) {
+    if (value) {
+      this.#cdr.detectChanges();
+      this.inputText.nativeElement.value = '';
+
+      const dataAtual = new Date();
+      const timestamp = dataAtual.getTime();
+      const id = `ID ${timestamp}`;
+
+      this.outputListItems.emit({
+        id,
+        checked: false,
+        value
+      });
+
+      return this.inputText.nativeElement.focus();
+    }
+  }
+}
